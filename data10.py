e5e673fb773c508015060d8af9643933d2f4c4d0
aginga <brianaginga35@gmail.com>
2022-10-15 10:54:46 +0300

    dta

---
+++ data10.py
@@ -0,0 +1,13 @@
+import csv
+
+file = open('data.csv', 'a')
+
+name = input('Enter name: ')
+phone = input('Enter phone number: ')
+county = input('Enter county: ')
+merger = input('Enter merger: ')
+
+writer = csv.writer(file)
+
+writer.writerow([name, phone, county, merger])
+
